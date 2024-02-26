10835ea1b9d0fdd287308acdabc91cd538a5b0e0
Jenniferwonder <Jenniferwonder@163.com>
2024-02-26 16:06:02 +0800

    remove unrelated files

--- feedback-app-practice/feedback-app/src/components/button/MyButton.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export default function MyButton() {
-	const [count, setCount] = useState(0);
-	function handleClick() {
-		setCount(count + 1);
-	}
-	return <button onClick={handleClick}>Clicked {count} times</button>;
-}
---
+++ mern-image-generation/README.md
@@ -0,0 +1,5 @@
+# Build and Deploy a Full Stack MERN AI Image Generation App  MidJourney & DALL E Clone
+![Image Generation App](https://i.ibb.co/p0f27C2/Thumbnail-9.png)
+
+### 
+### 
